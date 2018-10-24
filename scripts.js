0b6b13ac8c24c10fc09d7783fa01e77df18347be
Sam Giambalvo <samgiambalvo@gmail.com>
2018-10-23 21:37:33 -0700

    Styling updates

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function()
       // get inches and multiply by dpi
       var inW = parseFloat($("#inW").val());
       var pxW = (inW) * 75;
+      pxW = pxW.toFixed(2);
       $("#pxW").val(pxW);
       
       // disable other forms of input
@@ -12,6 +13,20 @@ $(document).ready(function()
       $(this).val(sanitized);
     }
     
+    function pxToIn()
+    {
+      // get px and divide by dpi
+      var pxW = parseFloat($("#pxW").val());
+      var inW = (pxW) / 75;
+      inW = inW.toFixed(2);
+      $("#inW").val(inW);
+
+      // disable other forms of input
+      var sanitized = $(this).val().replace(/[^0-9]/g, '');
+      $(this).val(sanitized);
+    }
+    
     //update values
     $(document).on("change, keyup", "#inW", inToPx);
+    $(document).on("change, keyup", "#pxW", pxToIn);
 });
