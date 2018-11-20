2d53daeeeac49744e842bb19e0b98d712fdce411
Sam Giambalvo <samgiambalvo@gmail.com>
2018-11-20 14:56:54 -0800

    New styling and layout

--- scripts.js
+++ scripts.js
@@ -5,12 +5,7 @@ $(document).ready(function()
       // get inches and multiply by dpi
       var inW = parseFloat($("#inW").val());
       var pxW = (inW) * 75;
-      pxW = pxW.toFixed(2);
       $("#pxW").val(pxW);
-      
-      // disable other forms of input
-      var sanitized = $(this).val().replace(/[^0-9]/g, '');
-      $(this).val(sanitized);
     }
     
     function pxToIn()
@@ -18,12 +13,7 @@ $(document).ready(function()
       // get px and divide by dpi
       var pxW = parseFloat($("#pxW").val());
       var inW = (pxW) / 75;
-      inW = inW.toFixed(2);
       $("#inW").val(inW);
-
-      // disable other forms of input
-      var sanitized = $(this).val().replace(/[^0-9]/g, '');
-      $(this).val(sanitized);
     }
     
     //update values
